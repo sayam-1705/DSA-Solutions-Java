8f4bd5b963a8e29c679cb8492c3d4d65a02dbed0
sayam-1705 <sayam2022mondal@gmail.com>
2025-05-21 00:15:33 +0530

    Pattern 4

---
+++ src/pattern/java/PatternFour.java
@@ -0,0 +1,22 @@
+package pattern.java;
+
+import java.util.Scanner;
+
+public class PatternFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                if (j >= i) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
